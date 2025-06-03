83a0ad82e65c795dee1d9ad7ca6b84e9c8e71959
zhaofenglee <zhaof_li@163.com>
2025-06-03 20:30:11 +0800

    Upgrade Abp to 9.2.0 and add bundling support for Cache Management in Blazor WebAssembly

--- src/JS.Abp.CacheManagement.Blazor.WebAssembly/CacheManagementBlazorWebAssemblyModule.cs
+++ src/JS.Abp.CacheManagement.Blazor.WebAssembly/CacheManagementBlazorWebAssemblyModule.cs
@@ -1,4 +1,5 @@
-﻿using Volo.Abp.AspNetCore.Components.WebAssembly.Theming;
+﻿using JS.Abp.CacheManagement.Blazor.WebAssembly.Bundling;
+using Volo.Abp.AspNetCore.Components.WebAssembly.Theming;
 using Volo.Abp.Modularity;
 
 namespace JS.Abp.CacheManagement.Blazor.WebAssembly;
@@ -6,7 +7,8 @@ namespace JS.Abp.CacheManagement.Blazor.WebAssembly;
 [DependsOn(
     typeof(CacheManagementBlazorModule),
     typeof(CacheManagementHttpApiClientModule),
-    typeof(AbpAspNetCoreComponentsWebAssemblyThemingModule)
+    typeof(AbpAspNetCoreComponentsWebAssemblyThemingModule),
+    typeof(CacheManagementBlazorWebAssemblyBundlingModule)
     )]
 public class CacheManagementBlazorWebAssemblyModule : AbpModule
 {
